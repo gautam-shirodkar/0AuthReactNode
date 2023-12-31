90fb27909e42ace16f9175ee2fa49c2ebdba01ac
Gautam <gautam.shirodkar92@gmail.com>
2023-12-31 08:42:08 +0530

    fixed issuer url

--- api/index.js
+++ api/index.js
@@ -14,19 +14,17 @@ const jwtCheck = auth({
 
 // app.use(jwtCheck);
 app.get("/", (req, res) => {
-  console.log("without auth success");
   res.status(200).send("success without auth");
 });
 
 app.get("/get-profile", jwtCheck, async (req, res) => {
-  const url = `https://${process.env.ISSUER}/userinfo`;
+  const url = `${process.env.ISSUER}userinfo`;
   const response = await fetch(url, {
     headers: {
       Authorization: `Bearer ${req.auth.token}`,
     },
   });
   const userInfo = await response.json();
-  console.log(userInfo);
   res.status(200).send(userInfo);
 });
 app.listen(4000, () => console.log("server running on port 4000"));
